9b566e3650c3750113632a08f94f03cfb8d247cc
Michael Peter <mipe.spam@gmail.com>
2024-11-13 00:36:35 +0100

    init: setup spring boot project and added some dependencies and database logic

---
+++ src/main/java/de/iammichaelpeter/airbnbclone/AirbnbCloneSpringBootBackendApplication.java
@@ -0,0 +1,13 @@
+package de.iammichaelpeter.airbnbclone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirbnbCloneSpringBootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirbnbCloneSpringBootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/iammichaelpeter/airbnbclone/AirbnbCloneSpringBootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.iammichaelpeter.airbnbclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneSpringBootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
